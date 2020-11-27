5677100728dfa5c183246ad541641aa8a5b965de
Olajide A. Hammed <olajide.a.hammed@gmail.com>
2020-11-27 05:31:59 +0100

    Update 0.0.1

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'rccg-flier-generator',
+    title: 'RCCG Flier Generator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -41,4 +41,4 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,93 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">rccg-flier-generator</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <canvas ref="canvas">Your browser does not support Canvas.</canvas>
+
+    <a @click="drawAvatar">[Draw]</a>
+    <a ref="downloadBtn" @click="download">Download</a>
   </div>
 </template>
 
 <script>
-export default {}
+  export default {
+    data() {
+      return {
+        ctx: null,
+        canvas: null,
+        background: {
+          src: '/img/rccg-1024.png',
+          width: 1024,
+          height: 1024,
+        },
+
+        avatar: {
+          top: 270, // centerX
+          left: 190, // centerY
+          width: 483, // radius
+        },
+      };
+    },
+
+    mounted() {
+      this.canvas = this.$refs.canvas;
+
+      this.canvas.height = this.background.height;
+      this.canvas.width = this.background.width;
+
+      this.ctx = this.canvas.getContext('2d');
+
+      this.drawBackground();
+    },
+
+    methods: {
+      drawBackground() {
+        if (this.canvas.getContext) {
+          // Draw Background Picture on canvas on 'image load'
+          const img = new Image();
+          img.onload = () => {
+            this.ctx.drawImage(img, 0, 0);
+          };
+          img.src = this.background.src;
+        }
+      },
+
+      drawAvatar() {
+        if (this.canvas.getContext) {
+          // Draw User avatar on canvas  on 'image load'
+          const img = new Image();
+          img.onload = () => {
+            // define a path
+            this.ctx.beginPath();
+            this.ctx.arc(
+              this.avatar.top + this.avatar.width / 2,
+              this.avatar.left + this.avatar.width / 2,
+              this.avatar.width / 2,
+              0,
+              2 * Math.PI
+            );
+            this.ctx.closePath();
+
+            // clip new drawings within this path
+            this.ctx.clip();
+            this.ctx.drawImage(
+              img,
+              this.avatar.top,
+              this.avatar.left,
+              this.avatar.width,
+              this.avatar.width
+            );
+          };
+          img.src = '/img/avatar.jpg';
+        }
+      },
+
+      download() {
+        const download = this.$refs.downloadBtn;
+        const image = this.canvas
+          .toDataURL('image/png')
+          .replace('image/png', 'image/octet-stream');
+        download.setAttribute('href', image);
+        download.setAttribute('download', 'flier.png');
+      },
+    },
+  };
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
