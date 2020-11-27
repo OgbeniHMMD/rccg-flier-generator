525334341d5f3cc44947fa95077a089f48397966
Olajide A. Hammed <olajide.a.hammed@gmail.com>
2020-11-27 08:55:53 +0100

    Improvement:
    - Added Loading Indicator Icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,5 @@ module.exports = {
       center: true,
       padding: '1rem',
     },
-
-    // extend: {
-    //   backgroundOpacity: {
-    //     10: '0.1',
-    //     25: '0.25',
-    //     50: '0.5',
-    //     95: '0.95',
-    //   },
-    // },
-  },
-
-  variants: {
-    extend: {
-      opacity: ['active'],
-    },
   },
 };
